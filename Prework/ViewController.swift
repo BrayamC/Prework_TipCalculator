cf129fa36fbef4ef10c3797efc85fdda90e9dfd8
Brayam Corral Flores <brayamcorral@gmail.com>
2021-01-01 17:07:25 -0800

    Added dark/light mode. Still need to modify segment color

--- Prework/ViewController.swift
+++ Prework/ViewController.swift
@@ -9,13 +9,23 @@ import UIKit
 
 class ViewController: UIViewController {
     
-    @IBOutlet weak var billAmountTextField: UITextField!
-    @IBOutlet weak var tipPercantageLabel: UILabel!
-    @IBOutlet weak var tipControl: UISegmentedControl!
-    @IBOutlet weak var totalLabel: UILabel!
-    @IBOutlet weak var tipSlider: UISlider!
+    @IBOutlet weak var colorSwitch: UISwitch!
     
-
+    @IBOutlet weak var BillAmountDesc: UILabel!
+    @IBOutlet weak var billAmountTextField: UITextField!
+    
+    @IBOutlet weak var tipPercantageDesc: UILabel!
+    @IBOutlet weak var tipPercantageLabel: UILabel!
+   
+    @IBOutlet weak var totalDesc: UILabel!
+    @IBOutlet weak var totalLabel: UILabel!
+    
+    @IBOutlet weak var tipSlider: UISlider!
+    @IBOutlet weak var tipControl: UISegmentedControl!
+    
+    
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
     }
@@ -56,7 +66,42 @@ class ViewController: UIViewController {
         // Update the tip and total labels
         tipPercantageLabel.text = String(format: "$%.2f", tip)
         totalLabel.text = String(format: "$%.2f", total)
+        
     }
+    
+    // Dark / light Mode
+    @IBAction func changeColor(_ sender: Any) {
+        
+        let white = UIColor(red: 255, green: 255, blue: 255, alpha: 1)
+        let black = UIColor(red: 0, green: 0, blue: 0, alpha: 1)
+        
+        if colorSwitch.isOn {
+            
+            self.view.backgroundColor = white
+            
+            BillAmountDesc.textColor = black
+            billAmountTextField.textColor = black
+            
+            tipPercantageDesc.textColor = black
+            tipPercantageLabel.textColor = black
+            
+            totalDesc.textColor = black
+            totalLabel.textColor = black
+            
+        } else {
+            self.view.backgroundColor = black
+            
+            BillAmountDesc.textColor = white
+            billAmountTextField.textColor = white
+            
+            tipPercantageDesc.textColor = white
+            tipPercantageLabel.textColor = white
+            
+            totalDesc.textColor = white
+            totalLabel.textColor = white
+        }
+    }
+    
     
     
 }
